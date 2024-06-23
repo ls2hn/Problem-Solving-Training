8c420ba51f7f6243a308e89461ca521e766a7e19
ls2hn <136432058+ls2hn@users.noreply.github.com>
2024-06-23 23:50:19 +0900

    Update 서울에서김서방찾기.py

--- Python/서울에서김서방찾기.py
+++ Python/서울에서김서방찾기.py
@@ -6,3 +6,13 @@ def solution(seoul):
             
     answer = f'김서방은 {key}에 있다'
     return answer
+
+# 다른 분들의 모범 코드
+# def solution(seoul):
+#     return "김서방은 {}에 있다".format(seoul.index('Kim'))
+
+# def solution(seoul):
+#     return f"김서방은 {seoul.index('Kim')}에 있다"
+
+# def solution(seoul):
+#     return ('김서방은 %d에 있다' %seoul.index('Kim'))
