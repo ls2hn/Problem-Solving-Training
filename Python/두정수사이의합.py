ccc17ec2e604c2bbbb85bfc494ca05bbdeffaa9c
ls2hn <136432058+ls2hn@users.noreply.github.com>
2024-06-27 18:48:48 +0900

    Update 두정수사이의합.py

--- Python/두정수사이의합.py
+++ Python/두정수사이의합.py
@@ -1,16 +1,34 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
+def solution(a, b):
+    answer=0
+    if a<=b:
+        for i in range(a, b+1):
+            answer += i
+    else:
+        for i in range(b, a+1):
+            answer +=i
+            
+    return i
 
-long long solution(int a, int b) {
-    long long answer = 0;
-    if(a > b){
-        int temp = b;
-        b = a;
-        a = temp;
-    }
-    for(int i=a;i<=b;i++){
-        answer += i;
-    }
-    return answer;
-}
+# # 참고 1
+# def solution(a, b):
+#     if a > b:
+#         a, b = b, a
+#     return sum(range(a, b + 1))
+
+# # 참고 2
+# def solution(a, b):
+#     return (abs(a-b)+1)*(a+b)//2
+
+# # 참고 3
+# def solution(a, b):
+#     return sum(range(min(a, b), max(a, b)+1))
+
+# # 참고 4
+# def solution(a, b):
+#     return sum(range(a,b+1) if a <= b else range(b,a+1))
+
+# # 참고 5
+# def solution(a, b):
+#     if(a>b):
+#         return solution(b,a)
+#     return sum([i for i in range(a,b+1)])
