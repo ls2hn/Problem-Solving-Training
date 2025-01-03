247e0df570a2101a3b27d0ad5bc8a0c1fad4bff3
ls2hn <136432058+ls2hn@users.noreply.github.com>
2025-01-04 00:00:01 +0900

    Update 133502_햄버거만들기.py

--- 프로그래머스/Lv.1/133502_햄버거만들기.py
+++ 프로그래머스/Lv.1/133502_햄버거만들기.py
@@ -1,11 +1,12 @@
 def solution(ingredient):
     answer = 0
     stack = []
-
+    
     for ing in ingredient:
         stack.append(ing)
-        if stack[-4:] == [1, 2, 3, 1]:
-            stack = stack[:-4]
+        # 마지막 4개만 확인하여 패턴 검사
+        if len(stack) >= 4 and stack[-4:] == [1, 2, 3, 1]:
+            stack[-4:] = []  # 패턴 제거
             answer += 1
-
+    
     return answer
