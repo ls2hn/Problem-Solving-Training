3201e8bd0269de95546dad1d7e18ecacd347a11f
ls2hn <136432058+ls2hn@users.noreply.github.com>
2025-01-14 23:26:27 +0900

    Create 1158_요세푸스문제.py

---
+++ 백준/Silver/1158_요세푸스문제.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+N, K = map(int, input().split())
+queue = deque(range(1, N + 1))
+result = []
+
+while queue:
+    queue.rotate(-(K - 1))
+    result.append(queue.popleft())
+
+print("<" + ", ".join(map(str, result)) + ">")
